e3e26f5af565bba9cabdf40202af2382ef8ad4e8
mooeezzyy2026 <mooeezzyy.1996@outlook.com>
2026-04-13 11:08:36 +0500

    Feature: Added Dummy Data Seeder and enhanced Peshawar Agri-GIS Dashboard

---
+++ seed_data.py
@@ -0,0 +1,74 @@
+import geopandas as gpd
+import pandas as pd
+from sqlalchemy import create_engine
+from shapely.geometry import Point, Polygon
+import random
+
+
+db_url = "postgresql://localhost:5432/moeezelahi"
+engine = create_engine(db_url)
+
+
+CENTER_LAT = 34.0151
+CENTER_LNG = 71.5249
+
+
+crop = ["wheat", "Maize", "Sugarcane", "Tobacco", "Vegetables"]
+
+farmers = ["Ali", "Ahmed", "Hassan", "Khan",
+           "Zaman", "Bilal", "Sami", "Faisal"]
+
+
+statuses = ["Healthy", "Sick", "Warning"]
+
+
+def seed_points(count=20):
+    points_data = []
+    for i in range(count):
+        lat = CENTER_LAT + random.uniform(-0.05, 0.05)
+        lng = CENTER_LNG + random.uniform(-0.05, 0.05)
+        point = Point(lng, lat)
+        points_data.append({
+            "owner_name": random.choice(farmers),
+            "crop_type": random.choice(crop),
+            "health_status": random.choice(statuses),
+            "geometry": Point(lng, lat)
+        })
+
+    gdf = gpd.GeoDataFrame(points_data, geometry='geometry', crs="EPSG:4326")
+
+    gdf.to_postgis("peshawar_reports", engine, if_exists="append")
+    print(f"UPLOADED {count} Random POINTS TO DATABASE.")
+
+
+def seed_polygons(count=10):
+    poly_data = []
+    for i in range(count):
+        lat = CENTER_LAT + random.uniform(-0.05, 0.05)
+        lng = CENTER_LNG + random.uniform(-0.05, 0.05)
+        size = 0.002
+
+        coords = [
+            (lng, lat),
+            (lng + size, lat),
+            (lng + size, lat + size),
+            (lng, lat + size),
+            (lng, lat)
+        ]
+        poly_data.append({
+            'owner_name': f"{random.choice(farmers)} Farm {i}",
+            'crop_type': random.choice(crop),
+            'geometry': Polygon(coords)
+        })
+
+    gdf_poly = gpd.GeoDataFrame(
+        poly_data, geometry='geometry', crs="EPSG:4326")
+
+    gdf_poly.to_postgis("farm_boundaries", engine, if_exists="append")
+    print(f"UPLOADED {count} Random POLYGONS TO DATABASE.")
+
+
+if __name__ == "__main__":
+    seed_points(20)
+    seed_polygons(10)
+    print("------- Full Scale data is been seeded into the database. -------")
